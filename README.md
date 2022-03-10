74c9c8c523e0211c1fb59a669b536b196c8a30f0
Kshitij A <kshitij.everest@gmail.com>
2022-03-10 18:58:54 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# movie-recommender-salsa
-Movie Recommendation using ML model.
+# Basic Movie Recommender
+### Basic content based movie recommendation. 
+
+To run the app: - 
+
+1. Run entire `movie_in.ipynb` notebook
+2. Run `pip install requirements.txt` in shell
+3. Run `streamlit run app.py` in shell
+
+
+
+Used basic feature extraction and count vectorization to generate vectors from tags and provide top 5 recommendations.
+
+
+### Kshitij Agarkar, IITN, 2021
+
+### Contact: -	
+
+<p align='center'>
+  <a href="mailto:bt20cse209@iiitn.ac.in">
+    <img src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white" />
+  </a>&nbsp;&nbsp;
+  <a href="https://instagram.com/kshitij_agarkar">
+    <img src="https://img.shields.io/badge/instagram-%23E4405F.svg?&style=for-the-badge&logo=instagram&logoColor=white" />        
+  </a>&nbsp;&nbsp;
+  <a href="https://github.com/horizon3902">
+    <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
+</p>
+
---
+++ app.py
@@ -0,0 +1,52 @@
+import os
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+movies_dict = pickle.load(open('data/moviedb_dict.pkl', 'rb'))
+movies_df = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('data/moviedb_similarity.pkl', 'rb'))
+
+def fetch_poster(movie_id):
+    API_KEY = os.getenv('TMDB_API_KEY')
+    response = requests.get(f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US")
+    data = response.json()
+    print(data)
+    return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+
+def recommend(movie):
+    movie_index = movies_df[movies_df['title']==movie].index[0]
+    distances = similarity[movie_index]
+    recommended_movies = sorted(list(enumerate(distances)), reverse=True, key=lambda k: k[1])[1:6]
+    rec_mov_list = []
+    rec_mov_posters = []
+    for i in recommended_movies:
+        movie_id = movies_df.iloc[i[0]]['movie_id']
+        rec_mov_list.append(movies_df.iloc[i[0]]['title'])
+        rec_mov_posters.append(fetch_poster(movie_id))
+    return rec_mov_list, rec_mov_posters
+
+st.title("Movie Recommendation App")
+
+fav_movie = st.selectbox(
+    'Enter your favourite movie',
+    movies_df['title'].values
+)
+
+if st.button('Recommend'):
+    rec_names, posters = recommend(fav_movie)
+    st.write(f'Based on your favourite movie, we recommend:')
+    
+    col1,col2,col3,col4,col5 = st.columns(5)
+    cols = (col1,col2,col3,col4,col5)
+    
+    for col in cols:
+        with col:
+            st.text(rec_names[cols.index(col)])
+            st.image(posters[cols.index(col)])
+
